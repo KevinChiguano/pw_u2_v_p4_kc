a5d3a57bf7b35a84cd74673f475a7fbb20cfe33e
KevinChiguano <kevin_chili1998@hotmail.com>
2023-07-06 11:21:53 -0500

    Juego Pokemon

--- src/modules/pokemon/pages/PokemonPages.vue
+++ src/modules/pokemon/pages/PokemonPages.vue
@@ -2,14 +2,24 @@
   <h1 v-if="!pokemonCorrecto">Espere por favor.....</h1>
   <div v-else>
     <h1>Juego Pokemon</h1>
+    <div class="contenedor-score">
+      <h1>Puntaje:{{ puntaje }}</h1>
+      <h1>Intentos: {{ intentos }}</h1>
+    </div>
     <PokemonImg :pokemonId="pokemonCorrecto.id" :muestraPokemon="showPokemon" />
     <PokemonOps
+      v-show="intentos < 3 && showPokemon==false"
       :opciones="pokemonArr"
       @seleccionado="revisarSeleccion($event)"
     />
-    <div>
-      <h1 v-if="showPokemon">Ganaste</h1>
-      <h1 v-else>Perdiste</h1>
+    <div
+      v-show="showPokemon || (intentos === 3 && !showPokemon)"
+      class="contenedor-fin"
+    >
+      <h1 v-if="showPokemon">Ganaste el Juego</h1>
+      <h1 v-if="showPokemon">Puntaje: {{ puntaje }}</h1>
+      <h1 v-if="!showPokemon && intentos === 3">Perdiste el Juego</h1>
+      <button v-on:click="reiniciar">Reiniciar</button>
     </div>
   </div>
 </template>
@@ -26,6 +36,8 @@ export default {
       pokemonArr: [],
       pokemonCorrecto: null,
       showPokemon: false,
+      puntaje: 0,
+      intentos: 0,
     };
   },
 
@@ -48,11 +60,27 @@ export default {
     revisarSeleccion(idSeleccionado) {
       console.log("Evento en el padre");
       console.log(idSeleccionado);
+      this.intentos++;
       if (idSeleccionado == this.pokemonCorrecto.id) {
         this.showPokemon = true;
+
+        if (this.intentos == 1) {
+          this.puntaje = 5;
+        } else if (this.intentos == 2) {
+          this.puntaje = 2;
+        } else if (this.intentos == 3) {
+          this.puntaje = 1;
+        }
       } else {
         this.showPokemon = false;
       }
+    },
+
+    async reiniciar() {
+      await this.cargaJuegoInicial();
+      this.showPokemon = false;
+      this.puntaje = 0;
+      this.intentos = 0;
     },
   },
 
@@ -69,4 +97,15 @@ export default {
 h1 {
   color: black;
 }
+
+.contenedor-score {
+  display: flex;
+  justify-content: center;
+}
+
+.contenedor-score h1 {
+  margin: 50px;
+}
+
+
 </style>
